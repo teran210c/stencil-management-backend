4449dfeef5bfb67dc36bf8c04091672112d782b2
teran210c <a242729@uach.mx>
2026-05-13 17:58:58 +0100

    Feat added getStencilsTotal function to controller

--- src/controllers/stencilsController.js
+++ src/controllers/stencilsController.js
@@ -2,16 +2,50 @@ const queries = require("../queries/stencilQueries")
 
 async function getStencils(req, res) {
     try {
+
         const stencils = await queries.getAllStencils()
+
         res.json(stencils)
+
     } catch(error) {
+
         console.error(error)
+
         res.status(500).json({
+
             error: "Error fetching stencils"
+
         })
+
     }   
+
 }
 
 module.exports = {
     getStencils
+}
+
+async function  getStencilsTotal(req, res) {
+
+    try {
+        
+        const total = await queries.getStencilsTotal()
+
+        res.json(total)
+
+
+    } catch (error) {
+
+        console.error(error)
+
+        res.status(500).json({
+            error: "Server error"
+        })
+        
+    }
+    
+}
+
+module.exports = {
+    getStencilsTotal
 }
